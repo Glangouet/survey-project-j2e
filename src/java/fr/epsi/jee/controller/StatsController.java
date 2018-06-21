0a9a63c41af346d5d97954d621bb5da26b4ca7af
Ludo Renzetti <ludovic.renzetti@gmail.com>
2018-06-21 15:19:14 +0200

    feature(bean): handle http get params

--- src/java/fr/epsi/jee/controller/StatsController.java
+++ src/java/fr/epsi/jee/controller/StatsController.java
@@ -5,30 +5,44 @@
  */
 package fr.epsi.jee.controller;
 
+import fr.epsi.jee.model.Question;
+import java.io.IOException;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import org.omnifaces.cdi.Param;
 
 /**
  *
  * @author Ludo Renzetti
  */
 @Named(value = "stats")
-@SessionScoped
+@RequestScoped
 @ManagedBean
 public class StatsController implements Serializable {
 
-    private String message = "Stats";
+    @Inject
+    @Param
+    private Integer uuid;
     
-    /**
-     * Creates a new instance of StatsController
-     */
+    @Inject
     public StatsController() {
     }
     
-    public String getMessage() {
-        return message;
+    public Integer getUUID() {
+        return uuid;
+    }
+    
+    public Question getQuestion() throws IOException {
+        if(uuid <= 0) {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/");
+            return null;
+        }
+        
+        return null;
     }
     
 }
