5d5b4b3e9911dbf4649fe193cf093e5e3e30575f
Ludo Renzetti <ludovic.renzetti@gmail.com>
2018-06-21 13:55:27 +0200

    refactor: new package architecture

--- src/java/fr/epsi/jee/model/Question.java
+++ src/java/fr/epsi/jee/model/Question.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package fr.epsi.jee.model;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
